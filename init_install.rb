5c19604e1ee20e3076efff83de935fc0b5188e8b
kk <sevkme@gmail.com>
2013-06-30 22:10:07 +0800

    add init script for system new install

---
+++ init_install.rb
@@ -0,0 +1,21 @@
+#install from mini.iso
+#
+
+a = "
+xfce4-terminal
+vim
+git
+xfonts-intl-chinese
+build-essential
+"
+a.each_line do |x|
+  system "apt-get install #{x}"
+end
+
+mkdir -p .irssi/scripts/autorun
+cd .irssi/scripts/autorun
+wget http://scripts.irssi.org/scripts/usercount.pl
+wget http://scripts.irssi.org/scripts/nickcolor.pl
+wget http://scripts.irssi.org/scripts/beep_beep.pl
+
+
